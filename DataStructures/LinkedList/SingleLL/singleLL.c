9c3341216f46a326dce5148ca63aaccfb7017593
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-04 11:37:47 +0530

    create 2nd node

--- DataStructures/LinkedList/SingleLL/singleLL.c
+++ DataStructures/LinkedList/SingleLL/singleLL.c
@@ -9,10 +9,15 @@ struct node{
 
 
 int main(){
-	struct node *head=NULL;
+	struct node *head=NULL;	//create head node
 	head = (struct node *)malloc(sizeof(struct node));
 	head->data=45;
 	head->link=NULL;
+	
+	struct node *current = malloc(sizeof(struct node));	//create 2nd node
+	current -> data = 98;
+	current -> link = NULL;
+	head -> link = current;	//link head node with current node
 	
 	printf("%d",head->data);
 	return 0;
