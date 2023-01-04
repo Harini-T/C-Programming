6988b3b809e5787a96f1d882281735f2933a37c0
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-04 14:43:24 +0530

    Deleting last node using 2 temp pointers

--- DataStructures/LinkedList/SingleLL/singleLL.c
+++ DataStructures/LinkedList/SingleLL/singleLL.c
@@ -115,6 +115,30 @@ struct node* del_first(struct node *head){
 	return head;
 }
 
+
+//Deleting last node using 2 temp pointers
+struct node* del_last(struct node *head){
+	if(head==NULL)
+		printf("List is already empty");
+	else if(head->link == NULL){
+		free(head);
+		head = NULL;
+	}
+	else{
+		struct node *temp = head;
+		struct node *temp2 = head;
+		while(temp->link != NULL){
+			temp2=temp;
+			temp=temp->link;
+		}
+		temp2 -> link = NULL;
+		free(temp);
+		temp = NULL;
+	}
+	return head;
+	
+}
+
 int main(){
 	
 	struct node *head=NULL;	//create head node
@@ -135,6 +159,8 @@ int main(){
 	add_at_pos(head,d,pos);
 	
 	head = del_first(head);
+	
+	head = del_last(head);
 	print_data(head);
 	return 0;
 }
