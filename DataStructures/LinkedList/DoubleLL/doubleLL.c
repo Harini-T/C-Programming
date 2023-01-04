e2e02292ea86f1ecd020f88a3e9999a4c9600f37
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-04 16:07:32 +0530

    Insertion at beginning

--- DataStructures/LinkedList/DoubleLL/doubleLL.c
+++ DataStructures/LinkedList/DoubleLL/doubleLL.c
@@ -12,18 +12,47 @@ struct node{
 struct node* addToEmpty(struct node* head, int data){
 	struct node *temp = malloc(sizeof(struct node));
 	temp -> prev = NULL;
-	temp -> data = 10;
+	temp -> data = data;
 	temp -> next = NULL;
 	head = temp;
 	return head;
 }
 
 
+//Insertion at beginning
+struct node* addAtBeg(struct node* head, int data){
+	struct node* temp = malloc(sizeof(struct node));
+	temp -> prev = NULL;
+	temp -> data = data;
+	temp -> next = NULL;
+	
+	temp -> next = head;
+	head -> prev = temp;
+	head = temp;
+	
+	return head;
+}
+
+//print all the elements of the linked list
+void print_data(struct node* head){
+	if(head == NULL){
+		printf("Linked List is empty");
+	}
+	struct node *ptr = head;
+	while(ptr!=NULL){
+		printf("%d --> ",ptr->data);
+		ptr=ptr->next;
+	}
+	printf(" NULL\n");
+}
+
 int main(){
 	
 	struct node* head = NULL;
 	head = addToEmpty(head,45);
-	printf("%d", head->data);
+	
+	head = addAtBeg(head,34);
+	print_data(head);
 	return 0;
 
 }
